176ce9947b8c45b612f2f1d780ba7c2462fead1e
Rizky Eko Putra <rizky.eko.putra@gmail.com>
2017-10-11 00:42:58 +0700

    Day 9: Recursion
    solved factorial algorithm

---
+++ 30 Days of Code Challenges/Day 9 Recursion/factorial.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int factorial(int n) {
+        // Complete this function
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = factorial(n);
+        Console.WriteLine(result);
+    }
+}

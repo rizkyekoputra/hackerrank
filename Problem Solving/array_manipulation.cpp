8a76a378021d851ffd3badf475090c3238bbf0bb
Ankit Raj <ankit.1998.ar@gmail.com>
2019-10-02 13:40:21 +0530

    Added array manipulation problem

---
+++ Problem Solving/array_manipulation.cpp
@@ -0,0 +1,96 @@
+// https://www.hackerrank.com/challenges/crush/problem
+// Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the array element between two given indices, inclusive. Once all operations have been performed, return the maximum value in your array.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the arrayManipulation function below.
+long arrayManipulation(int n, vector<vector<int>> queries, long m) {
+    vector<long> arr(n);
+    for(int i=0;i<n;i++){
+        arr.push_back(0);
+    }
+    for(int i = 0;i<m;i++){
+    	// Read the values
+        long left = queries[i][0];
+        long right = queries[i][1];
+        long k = queries[i][2];
+        arr[left-1] += k; //add value k to left index
+        arr[right] += -k; //subtract k in the right index
+        // for(int z:arr){
+        //     cout << z << " ";
+        // }
+        // cout << endl;
+    }
+    for(int i = 1; i<n;i++){
+        arr[i] += arr[i-1]; //keep adding the previous index value until you reach right index
+    }
+    // for(int z:arr){
+    //     cout << z << " ";
+    // }
+    return *max_element(arr.begin(), arr.end());
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nm_temp;
+    getline(cin, nm_temp);
+
+    vector<string> nm = split_string(nm_temp);
+
+    int n = stoi(nm[0]);
+
+    int m = stoi(nm[1]);
+
+    vector<vector<int>> queries(m);
+    for (int i = 0; i < m; i++) {
+        queries[i].resize(3);
+
+        for (int j = 0; j < 3; j++) {
+            cin >> queries[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    long result = arrayManipulation(n, queries,m);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}

b270b85ece68fc3eebf1d78d4fc6ed2f12dc4713
Rizky Eko Putra <rizky.eko.putra@gmail.com>
2019-10-02 13:23:35 +0700

    Day 17 More Exceptions

---
+++ 30 Days of Code Challenges/Day 17 More Exceptions.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+//Write your code here
+class Calculator{
+    public static int power(int n, int p) throws Exception{
+        if (n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+        }
+        int result = 1;
+        while (p > 0){
+            result = result*n;
+            p -= 1;
+        }
+        return result;
+    }
+}
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
